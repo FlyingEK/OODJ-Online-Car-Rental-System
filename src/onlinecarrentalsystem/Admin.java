8e87f919ece150460e55013b7203d0bbf9bc6039
Yee Kay <yeekay2002@hotmail.com>
2022-12-03 11:45:18 +0800

    ek

---
+++ src/onlinecarrentalsystem/Admin.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onlinecarrentalsystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class Admin extends User {
+
+    public Admin(){
+        User u = new User("admin");
+    }
+    
+
+    
+    
+}
---
+++ src/onlinecarrentalsystem/User.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onlinecarrentalsystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class User {
+    protected String ID, username, password;
+    
+    public User(String userType){
+       //tak tau buat mcm not working
+       ArrayList<String> user = new ArrayList<String>();
+       if (userType.toLowerCase().equals("admin")){
+           FileHandling fh = new FileHandling(); 
+           user = fh.readFile("admin.txt");
+       } else if (userType.toLowerCase().equals("customer")) {
+           FileHandling fh = new FileHandling(); 
+           user = fh.readFile("admin.txt");
+       }
+       //else/ if not exist display error?
+        String[] splitUser = user.get(0).split(";");
+        ID = splitUser[0];
+        username = splitUser[1];
+        password = splitUser[2];
+    }
+    
+    public static Boolean login(){
+    }
+    
+}
